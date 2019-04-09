085b3824aa629ff2e13536cecca6faad6365745b
sabrina cos <sabrina.b.cos@gmail.com>
2019-04-09 15:31:53 +0200

    ajout CMetierFicheFrais

---
+++ src/Service/CMetierFicheFrais.java
@@ -0,0 +1,46 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Service;
+
+/**
+ *
+ * @Sabrina Cos
+ */
+public class CMetierFicheFrais {
+    
+    CTableFicheFrais tablefichefrais;
+    CTableTypeFrais tabletypefrais;
+    CTableInclure tableinclure;
+
+    public CTableFicheFrais getTablefichefrais() {
+        return tablefichefrais;
+    }
+
+    public void setTablefichefrais(CTableFicheFrais tablefichefrais) {
+        this.tablefichefrais = tablefichefrais;
+    }
+
+    public CTableTypeFrais getTabletypefrais() {
+        return tabletypefrais;
+    }
+
+    public void setTabletypefrais(CTableTypeFrais tabletypefrais) {
+        this.tabletypefrais = tabletypefrais;
+    }
+
+    public CTableInclure getTableinclure() {
+        return tableinclure;
+    }
+
+    public void setTableinclure(CTableInclure tableinclure) {
+        this.tableinclure = tableinclure;
+    }
+    
+
+    
+    
+    
+}
